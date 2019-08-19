6a6babc1433cd23ac12caf282c6f1a001e69ad60
Ignacy Wojdylo <ignacy.wojdylo@gmail.com>
2019-08-19 12:52:27 +0200

    delete creator property from Photo model (and drop photos and users db to reset all

--- models/Photo.js
+++ models/Photo.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
 
 const photoSchema = new Schema({
   imageUrl: {
     type: String,
     required: true,
     unique: true
-  },
-  creator: {
-    type: ObjectId,
-    ref: 'User',
-    required: true,
   },
   category: {
     type: String,
